9bd231871a8293a87ffb6bed4a7674ac07eca1ad
Li <varlamova_e.t@mail.ru>
2023-03-14 19:53:56 +0300

    dfb

---
+++ Notebook.java
@@ -0,0 +1,13 @@
+public class Notebook {
+    private int cost;
+    private int memory;
+    
+    public Notebook(int cost, int memory) {
+        this.cost = cost;
+        this.memory = memory;
+    }
+    
+    public int getCost() {
+        return cost;
+    }
+}
